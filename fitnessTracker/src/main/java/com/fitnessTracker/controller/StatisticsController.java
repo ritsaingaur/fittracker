15be2f4d895ac45fc8eda75500004e432876d44f
ritsaingaur <122271562+ritsaingaur@users.noreply.github.com>
2025-10-08 15:41:50 +0530

    Update StatisticsController.java

--- fitnessTracker/src/main/java/com/fitnessTracker/controller/StatisticsController.java
+++ fitnessTracker/src/main/java/com/fitnessTracker/controller/StatisticsController.java
@@ -3,6 +3,7 @@ package com.fitnessTracker.controller;
 import com.fitnessTracker.dto.DashboardResponse;
 import com.fitnessTracker.dto.UserHistoryResponse;
 import com.fitnessTracker.service.StatisticsService;
+import com.fitnessTracker.dto.HomepageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,13 @@ public class StatisticsController {
     public ResponseEntity<UserHistoryResponse> getUserHistory(@PathVariable String username) {
         UserHistoryResponse history = statisticsService.getUserHistory(username);
         return ResponseEntity.ok(history);
+    }
+
+
+
+    @GetMapping("/homepage/{username}")
+    public ResponseEntity<HomepageResponse> getHomepageData(@PathVariable String username) {
+        HomepageResponse homepageData = statisticsService.getHomepageData(username);
+        return ResponseEntity.ok(homepageData);
     }
 }
